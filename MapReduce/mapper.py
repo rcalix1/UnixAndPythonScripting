6a0a80b201967926cbe44eebb0b707c221f43886
Ricardo Calix <rcalix1@gmail.com>
2020-03-25 14:10:41 -0400

    Add files via upload

---
+++ MapReduce/mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    words = line.split()
+    #print words
+    for word in words:
+        print '%s \t %s' % (word, 1)
