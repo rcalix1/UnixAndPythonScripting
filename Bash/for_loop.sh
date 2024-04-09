f1a32d6857363854617b5ba1e688987bd1c5132f
Ricardo Calix <rcalix1@gmail.com>
2024-04-09 15:24:06 -0400

    Add files via upload

---
+++ Bash/for_loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# use bash instead of sh or dash
+
+for (( n=2; n<=10; n++ ))
+do
+echo "$n seconds"
+done
---
+++ Bash/while_loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## use bash instead of dash or sh
+
+n=0
+while :
+do
+     echo "Countdown: $n"
+     ((n++))
+done
